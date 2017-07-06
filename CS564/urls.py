c6f642195d7d3db4ce0f4bdd115f35d4439ea3c2
Jeremy West <jemofthewest@gmail.com>
2017-07-05 20:03:45 -0500

    Create an index view

--- CS564/urls.py
+++ CS564/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+
 # from django.contrib import admin
-from readgood.views import BookList
+from readgood.views import BookList, index
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
+    url(r'^$', index, name='index'),
     url(r'^books/$', BookList.as_view(), name='books'),
 ]
--- readgood/views.py
+++ readgood/views.py
@@ -1,8 +1,14 @@
+from django.shortcuts import render
 from django.views.generic import ListView
+
 from readgood.models import Book
 
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
 class BookList(ListView):
     template_name = 'readgood/book_list.html'
     queryset = Book.objects.all().order_by('title')
