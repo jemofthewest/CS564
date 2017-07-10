676669ea3efc8c78c184518ea9e50301ffa1e1c1
Jeremy West <jemofthewest@gmail.com>
2017-07-10 12:45:52 -0500

    Add author and publisher models

--- readgood/models.py
+++ readgood/models.py
@@ -4,13 +4,24 @@ from django.db import models
 # Create your models here.
 class Book(models.Model):
     isbn = models.CharField(primary_key=True, max_length=13)
-    title = models.TextField(blank=True, null=True)
-    author = models.TextField(blank=True, null=True)
+    title = models.CharField(blank=True, null=True, max_length=255)
+    author = models.CharField(blank=True, null=True, max_length=255)
     year_published = models.IntegerField(blank=True, null=True)
-    publisher = models.TextField(blank=True, null=True)
+    publisher = models.CharField(blank=True, null=True, max_length=255)
     image_url_s = models.URLField(blank=True, null=True)
     image_url_m = models.URLField(blank=True, null=True)
     image_url_l = models.URLField(blank=True, null=True)
+    slug = models.SlugField(max_length=255)
 
     def __str__(self):
         return self.title
+
+
+class Author(models.Model):
+    name = models.CharField(primary_key=True, max_length=255)
+    slug = models.SlugField(max_length=255)
+
+
+class Publisher(models.Model):
+    name = models.CharField(primary_key=True, max_length=255)
+    slug = models.SlugField(max_length=255)
