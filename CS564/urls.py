f0c64e13cf4947306071775cbb5bcc7a994a55c2
Jeremy West <jemofthewest@gmail.com>
2017-07-13 21:40:33 -0500

    Enable user auth views

--- CS564/urls.py
+++ CS564/urls.py
@@ -13,13 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 
 # from django.contrib import admin
 from readgood.views import *
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
+    url('^', include('django.contrib.auth.urls')),
     url(r'^$', index, name='index'),
     url(r'^books/$', BookList.as_view(), name='books'),
     url(r'^book/(?P<pk>[0-9]+X?)/$', BookDetail.as_view(), name='book_pk'),
